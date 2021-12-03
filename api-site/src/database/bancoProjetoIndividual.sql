fdfe34cad55369a4da9dd402e997b589526ce824
Mariana Medina <mariana.medina@bandtec.com.br>
2021-12-02 23:15:06 -0300

    subindo os quizes

---
+++ api-site/src/database/bancoProjetoIndividual.sql
@@ -0,0 +1,15 @@
+select * from culinaria;
+
+
+insert into culinaria(nome,img)values
+	('culinaria brasileira', 'https://cdn.awsli.com.br/600x450/1420/1420494/produto/90203937/273bd9bafa.jpg'),
+    ('culinaria mexicana' , 'https://images.pexels.com/photos/7388096/pexels-photo-7388096.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+    ('culinaria italiana', 'https://images.pexels.com/photos/6659612/pexels-photo-6659612.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+    ('culinaria arabe', 'https://catracalivre.com.br/wp-content/uploads/sites/8/2016/10/habibs-2.jpg');
+    
+    select * from curtidas;
+    
+  select * from usuario;
+  
+  
+    
