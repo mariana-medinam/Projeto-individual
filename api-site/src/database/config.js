c2918129f240ed1205ae9d6258120a9ae8871147
Mariana Medina <mariana.medina@bandtec.com.br>
2022-03-06 11:01:42 -0300

    subindo ultimas alterações

--- api-site/src/database/config.js
+++ api-site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     user: "root",
     database: "projetoIndividual",
-    password: "YES",
+    password: "Mariana10*",
 };
 
 function executar(instrucao) {
